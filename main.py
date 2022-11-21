0e4bd0187e4331ab7820e90f96850d3dea8bc041
Rangoiv <rangoiv@gmail.com>
2022-11-21 01:03:21 +0100

    Added get similarity matrix function

--- main.py
+++ main.py
@@ -26,15 +26,25 @@ def get_neighborhood_graph(ind, edges):
     secondCol = edges[:, 1]
     print()
     vertices = np.concatenate([
+        [ind],
         firstCol[secondCol == ind],
-        secondCol[firstCol == ind],
-        [ind]
+        secondCol[firstCol == ind]
     ])
     vertices = np.unique(np.sort(vertices))
     vertices_set = set(vertices)
     edges_with_ind = np.asarray([[i, j] for i, j in edges if i in vertices_set and j in vertices_set])
 
     return vertices, edges_with_ind
+
+
+def get_similarity_matrix(A, B):
+    Z1 = np.array([[1 for i in range(len(A))] for j in range(len(B))])
+    Zk = Z1
+    print("Starting iterations")
+    for i in range(100):
+        Zk = iterate(Zk, A, B)
+        Zk = iterate(Zk, A, B)
+    return Zk
 
 
 def iterate(Zk, A, B):
@@ -54,7 +64,7 @@ def draw_graph(vertices, names, edges):
                                    overlap='scale'),
                    node_attr=dict(shape='plaintext',
                                   margin='0',
-                                  fontsize='10',),
+                                  fontsize='10', ),
                    edge_attr=dict(arrowsize='0.4'))
     g.engine = 'circo'
     for v in vertices:
@@ -63,6 +73,7 @@ def draw_graph(vertices, names, edges):
         g.edge(str(e[0]), str(e[1]))
     print("viewing")
     g.view()
+
 
 # =================== START ========================
 
@@ -83,7 +94,6 @@ def main():
 
     vertices_mappings = dict((j, i) for i, j in enumerate(vertices))
     # draw_graph(vertices, words, edges)
-
     n = len(vertices)
     edges_matrix = [[0 for i in range(n)] for j in range(n)]
     for i, j in edges:
@@ -91,28 +101,18 @@ def main():
     edges_matrix = np.array(edges_matrix)
 
     A = np.array([[0, 1, 0], [0, 0, 1], [0, 0, 0]])
-    print(A)
-
-    Z1 = np.array([[1 for i in range(3)] for j in range(n)])
-
-    Zk = Z1
-    print("Starting iterations")
-    for i in range(100):
-        Zk = iterate(Zk, A, edges_matrix)
-        Zk = iterate(Zk, A, edges_matrix)
-
-    similarity_scores = [i[1] for i in Zk]
+    similarity_matrix = get_similarity_matrix(A, edges_matrix)
+    similarity_scores = [i[1] for i in similarity_matrix]
     similarity_scores = [(j, i) for (i, j) in enumerate(similarity_scores)]
     similarity_scores.sort(reverse=True)
 
     reverse_mappings = dict(enumerate(vertices))
-    vertices_set = set(vertices)
     for i, j in similarity_scores[:30]:
-        h=reverse_mappings[j]
-        print(i, words[h])
-        # l1 = len(edges[edges[:,1]==h])
-        # l2 = len(edges[edges[:,0]==h])
-        # print(i, "|", l1, " -> ", words[h], " -> ", l2, " = ", l1+l2)
+        h = reverse_mappings[j]
+        # print(i, words[h])
+        l1 = len(edges[edges[:, 1] == h])
+        l2 = len(edges[edges[:, 0] == h])
+        print(i, "|", l1, " -> ", words[h], " -> ", l2, " = ", l1 + l2)
 
 
 main()
